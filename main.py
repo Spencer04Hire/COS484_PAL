c8ec7b4a03cc6149e95fca09700881e76ad07724
Spencer04Hire <124114480+Spencer04Hire@users.noreply.github.com>
2025-05-01 19:43:56 -0400

    Create script to run sample queries for all question types

---
+++ main.py
@@ -0,0 +1,100 @@
+import openai
+import pal
+from pal.prompt import algorithmic_prompt, colored_object_prompt, date_understanding_prompt, math_prompts, penguin_prompt
+from pal.core.runtime import DateRuntime, JavaRuntime
+
+MODEL = 'gpt-4o-mini'
+
+
+interface = pal.interface.ProgramChatInterface(model=MODEL, get_answer_expr='solution()', verbose=True, runtime=JavaRuntime())
+
+question = "say python twice and data once, and then repeat all of this three times."
+prompt = algorithmic_prompt.REPEAT_COPY_PROMPT.format(question=question)
+
+answer = interface.run(prompt, time_out=10)
+
+# Returns python python data python python data python python data
+print('\n==========================')
+print(f'The answer is {answer}.')
+print('==========================\n')
+
+exit()
+
+
+
+
+
+
+
+
+interface = pal.interface.ProgramChatInterface(model=MODEL, get_answer_expr='solution()', verbose=True)
+
+question = "I have an apple, three bananas, a strawberry, a peach, three oranges, a plum, a raspberry, two grapes, a nectarine, and a blackberry. How many fruits do I have?"
+prompt = algorithmic_prompt.OBJECT_COUNTING_PROMPTING.format(question=question)
+
+
+answer = interface.run(prompt, time_out=10)
+
+# Returns 15
+print('\n==========================')
+print(f'The answer is {answer}.')
+print('==========================\n')
+
+
+
+interface = pal.interface.ProgramChatInterface(model=MODEL, get_answer_symbol='answer', verbose=True)
+
+question = "On the nightstand, you see a mauve stress ball and a purple booklet. What color is the booklet?"
+prompt = colored_object_prompt.COLOR_OBJECT_PROMPTS["Java"].format(question=question)
+
+answer = interface.run(prompt, time_out=10)
+
+# Returns purple
+print('\n==========================')
+print(f'The answer is {answer}.')
+print('==========================\n')
+
+
+
+
+
+
+interface = pal.interface.ProgramChatInterface(model=MODEL, get_answer_symbol='answer', verbose=True, runtime=DateRuntime())
+
+question = "Yesterday was April 30, 2021. What is the date today in MM/DD/YYYY?"
+prompt = date_understanding_prompt.DATE_UNDERSTANDING_PROMPT.format(question=question)
+
+answer = interface.run(prompt, time_out=10)
+
+# Returns 05/01/2021
+print('\n==========================')
+print(f'The answer is {answer}.')
+print('==========================\n')
+
+
+
+
+interface = pal.interface.ProgramChatInterface(model=MODEL, get_answer_expr='solution()', verbose=True)
+
+question = "Mrs. Hilt met 15 friends. Nine of the friends were carrying pears. The rest were carrying oranges. How many friends were carrying oranges?"
+prompt = math_prompts.MATH_PROMPT.format(question=question)
+
+answer = interface.run(prompt, time_out=10)
+
+# Should be 6
+print('\n==========================')
+print(f'The answer is {answer}.')
+print('==========================\n')
+
+
+interface = pal.interface.ProgramChatInterface(model=MODEL, get_answer_symbol='answer', verbose=True)
+
+question = "Here is a table where the first line is a header and each subsequent line is a penguin:\n\nname, age, height (cm), weight (kg)\nLouis, 7, 50, 11\nBernard, 5, 80, 13\nVincent, 9, 60, 11\nGwen, 8, 70, 15\n\nFor example: the age of Louis is 7, the weight of Gwen is 15 kg, the height of Bernard is 80 cm.\nWhat is the age of Bernard?"
+prompt = penguin_prompt.PENGUIN_PROMPT.format(question=question)
+
+answer = interface.run(prompt, time_out=10)
+
+# Should be 5
+print('\n==========================')
+print(f'The answer is {answer}.')
+print('==========================\n')
